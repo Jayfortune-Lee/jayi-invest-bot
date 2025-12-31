a4a6f5c8e29fa02ff5d3d9cd5a6bd7df59e0ae80
Jayfortune-Lee <tnbtgxzx7w@privaterelay.appleid.com>
2025-12-31 15:33:43 +0900

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,8 +1,15 @@
 import os
 import requests
 
-def send_telegram_message(bot_token: str, chat_id: str, message: str):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    payload = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
+TG_TOKEN = os.getenv("TG_TOKEN")
+TG_ID = os.getenv("TG_ID")
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TG_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
     response = requests.post(url, data=payload)
     return response.json()
