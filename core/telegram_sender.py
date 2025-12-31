a101703ab0a4cb51fd9e559bf4f468c3f3993440
Jayfortune-Lee <tnbtgxzx7w@privaterelay.appleid.com>
2025-12-31 09:52:39 +0900

    Update telegram_sender.py

--- core/telegram_sender.py
+++ core/telegram_sender.py
@@ -2,4 +2,4 @@ from telegram import Bot
 
 def send_telegram_message(message: str, token: str, chat_id: str):
     bot = Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=message)
+    bot.send_message(chat_id=chat_id, text=message, parse_mode="Markdown")
