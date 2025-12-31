4391d995359e78a926a9b855523321639c3438aa
Jayfortune-Lee <tnbtgxzx7w@privaterelay.appleid.com>
2025-12-31 10:06:06 +0900

    Update auction_bot.py

--- bots/auction_bot.py
+++ bots/auction_bot.py
@@ -1,1 +1,15 @@
 # Auction Jayi Bot (서울 아파트 경매)
+import os
+from core.auction import get_auction_message
+from telegram_sender import send_telegram_message
+
+def run_auction_bot():
+    auction_msg = get_auction_message()
+    send_telegram_message(
+        auction_msg,
+        token=os.environ["TG_TOKEN_AUCTION"],
+        chat_id=os.environ["TG_ID"]
+    )
+
+if __name__ == "__main__":
+    run_auction_bot()
