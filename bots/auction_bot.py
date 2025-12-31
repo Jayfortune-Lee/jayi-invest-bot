f96b714f743468ee0c9720c2275a81547f045195
Jayfortune-Lee <tnbtgxzx7w@privaterelay.appleid.com>
2025-12-31 15:12:29 +0900

    Update auction_bot.py

--- bots/auction_bot.py
+++ bots/auction_bot.py
@@ -1,11 +1,14 @@
+from core.auction import generate_auction_brief
+from telegram_sender import send_telegram_message
 import os
-from core.auction import get_auction_brief
-from core.telegram_sender import send_telegram_message
+
+BOT_TOKEN = os.getenv("TG_TOKEN_AUCTION")
+CHAT_ID = os.getenv("TG_ID_AUCTION")
 
 def run_auction_bot():
-    message = get_auction_brief()
-    chat_id = os.environ.get("TG_ID_AUCTION")
-    send_telegram_message(chat_id, message)
+    auction_brief = generate_auction_brief()
+    message = f"📅 오늘 서울 경매 브리핑\n━━━━━━━━━━━━━━━━━━\n{auction_brief}"
+    send_telegram_message(BOT_TOKEN, CHAT_ID, message)
 
 if __name__ == "__main__":
     run_auction_bot()
